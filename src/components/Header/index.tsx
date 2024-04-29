51d74b3bb38df47ea246fdf042ffa36dcbb06b2b
Pedro Vitor <pedrovitor2110@gmail.com>
2024-04-29 17:56:37 -0300

    feat: component Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+        <header className="arial bg-secondary-orange pt-10 pb-8 mb-3">
+            <div className="container mx-auto px-4">
+                <h1 className="primary-orange text-5xl font-bold mb-2">Logaroo</h1>
+                <h2 className="uppercase font-bold text-xs">[Teste] Front-end</h2>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
